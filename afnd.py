f8c161690f6586a6f806b69fe710a7b7bb029bc0
Maverickk63 <103067766+Maverickk63@users.noreply.github.com>
2024-10-03 22:06:11 -0300

    Update nos comentários

--- afnd.py
+++ afnd.py
@@ -53,25 +53,25 @@ def converter_AFND_AFD():
     while estados_criados > 0:
         estados_criados = 0 #Ele loopa tentando criar estados frutos das transições dos estados
         for estado in AFD:
-            if estado['se_1'] == 'não testado':
-                passo_vazio = transicao_vazia(estado['estados_superposicao'])
-                passos_primario = list(set(passo_vazio) | set(estado['estados_superposicao']))
-                passos_um , passos_zero = transicao_normal(passos_primario)
+            if estado['se_1'] == 'não testado': # Verifica se o estado é novo , se for vai ter 'não testado' salvo no armazenamento de transição
+                passo_vazio = transicao_vazia(estado['estados_superposicao']) # Testa os passos que são dados se vazio
+                passos_primario = list(set(passo_vazio) | set(estado['estados_superposicao'])) # junta isso com os estados que coompoem este estado 
+                passos_um , passos_zero = transicao_normal(passos_primario) # faz a transição normal (se leu 1 ou 0)
                 if passos_um:
-                    passos_um = sorted(list(set(passos_um) | set(transicao_vazia(passos_um))))
-                    nome_passos_um = "".join(passos_um)
-                    estado['se_1'] = nome_passos_um
+                    passos_um = sorted(list(set(passos_um) | set(transicao_vazia(passos_um)))) # cria uma lista com os estados do caso 1 com os estados ao andar vazio
+                    nome_passos_um = "".join(passos_um) # nome é o concat de todos os estados
+                    estado['se_1'] = nome_passos_um # se 1 vai para esse estado
                     n_tem = 1
-                    for x in AFD:
-                        if nome_passos_um == x['nome']:
+                    for x in AFD: # Teste se estado ja existe
+                        if nome_passos_um == x['nome']: # caça em todos os estados da lista AFD
                             n_tem = 0
                     if n_tem == 1:
-                        criar_Estado_Composto(nome_passos_um, passos_um)
+                        criar_Estado_Composto(nome_passos_um, passos_um) # se não tem cria o estado
                         estados_criados += 1 # Se um estado novo foi criado o loop continua para passar por ele
                 else:
-                    estado['se_1'] = 'testado'
+                    estado['se_1'] = 'testado' # Caso n aja transição ao ler 1
                 if passos_zero:
-                    passos_zero = sorted(list(set(passos_zero) | set(transicao_vazia(passos_zero))))
+                    passos_zero = sorted(list(set(passos_zero) | set(transicao_vazia(passos_zero)))) # mesma coisa de cima so que para se 0
                     nome_passos_zero = "".join(passos_zero)
                     estado['se_0'] = nome_passos_zero
                     n_tem = 1
@@ -85,11 +85,11 @@ def converter_AFND_AFD():
                     estado['se_0'] = 'testado'
 
 def transicao_vazia(estados): # Recebe array dos estados compostos
-    caminhovazio = []
-    for estado in estados:
-        for transicao in AFND:
-            if transicao['nome'] == estado:
-                if transicao['se_h']:
+    caminhovazio = [] # salva os estados q passou quando vazio
+    for estado in estados: # passa em todos os estados que coompoem o composto
+        for transicao in AFND:  # passa por todos os do AFND
+            if transicao['nome'] == estado: # acha o estado que coompoem no AFND
+                if transicao['se_h']: #cruza esses  dados para achar transição vazia
                     # Se_H é array tem q pegar cada valor individual do array e n o array inteiro , NÃO PODE TER VALOR REPETIDO NO caminhovazio
                      for destino in transicao['se_h']:
                         repete = 0
@@ -97,10 +97,10 @@ def transicao_vazia(estados): # Recebe array dos estados compostos
                             if(destino == x):
                                 repete += 1
                         if repete == 0:
-                            caminhovazio.append(destino)
+                            caminhovazio.append(destino) # adiciona o passo vazio ao array se ele ja não estiver no array
     if caminhovazio:
         caminhovazio_tamanho = 0
-        while len(caminhovazio) != caminhovazio_tamanho:
+        while len(caminhovazio) != caminhovazio_tamanho: # repete o mesmo processo ate o array parar de crescer (ou seja ja adicionou todos os passos vazios)
             caminhovazio_tamanho = len(caminhovazio)
             for estado in caminhovazio:
                 for transicao in AFND:
@@ -118,7 +118,7 @@ def transicao_vazia(estados): # Recebe array dos estados compostos
 def transicao_normal(estados):
     caminho_um = []
     caminho_zero = []
-    for estado in estados:
+    for estado in estados: # mesma logica que o caminho vazio mas sem repetir pq so precisa passar uma vez
         for transicao in AFND:
             if transicao['nome'] == estado:
                 if transicao['se_1']:
@@ -130,7 +130,7 @@ def transicao_normal(estados):
                                 repete += 1
                         if repete == 0:
                             caminho_um.append(destino)
-    for estado in estados:
+    for estado in estados: # O mesmo para zero
         for transicao in AFND:
             if transicao['nome'] == estado:
                 if transicao['se_0']:
@@ -156,13 +156,13 @@ def criar_Estado_Composto(nome, estados): # Criar um valor no dicionario de esta
 def salvar_AFD(arqv):
     with open(arqv + "_AFD.txt",'x') as salvar:
         linha_0 = ""
-        for estado in AFD:
-            linha_0 = linha_0 + estado['nome'] + " "
+        for estado in AFD: # O nome de cada estado em AFD é salvo na linha zero
+            linha_0 = linha_0 + estado['nome'] + " " 
         salvar.write(linha_0 + "\n")
         global EstadoInicialAFD
-        salvar.write(EstadoInicialAFD + "\n")
+        salvar.write(EstadoInicialAFD + "\n") # Estado inicial na linha 1
         linha_2 = ""
-        for estado in EstadoFinalAFD:
+        for estado in EstadoFinalAFD: # Estados finais na linha 2
             linha_2 = linha_2 + estado + " "
         salvar.write(linha_2 + "\n")
         # Inserir as transições 
@@ -176,38 +176,38 @@ def definir_Estado_Final_AFD():
     global EstadoFinal, EstadoFinalAFD
     for finalN in EstadoFinal:
         for estado in AFD:
-            for subEstado in estado['estados_superposicao']:
+            for subEstado in estado['estados_superposicao']: # Se o estado composto contem um estado final AFND ele é Final
                 if finalN == subEstado:
                     EstadoFinalAFD.append(estado['nome'])
     EstadoFinalAFD = sorted(list(set(EstadoFinalAFD))) # Set tira repetido , list faz isso voltar a ser uma lista , sorted e pra organizar 
 
 def maquina_AFD(palavra):
-    EstadoAtual = EstadoInicialAFD
-    for letra in palavra:
+    EstadoAtual = EstadoInicialAFD # Inicia a maquina com o estado inicial do AFD
+    for letra in palavra: #Le cada letra da palavra e vai testando
         if letra == "1":
-            for estado in AFD:
-                if estado['nome'] == EstadoAtual:
-                    EstadoAtual = estado['se_1']
+            for estado in AFD: # passa em todos os AFD
+                if estado['nome'] == EstadoAtual: 
+                    EstadoAtual = estado['se_1'] # Se encontrar o estado atual pega o se 1 dele e continua
                     break
         elif letra == "0":
-            for estado in AFD:
+            for estado in AFD: # passa em todos os AFD
                 if estado['nome'] == EstadoAtual:
-                    EstadoAtual = estado['se_0']
+                    EstadoAtual = estado['se_0'] # Se encontrar o estado atual pega o se 0 dele e continua
                     break
         else:
-            return "não aceito"
-        if EstadoAtual == "testado":
-            return "não aceito"
+            return "não aceito" # Caso de letra fora do alfabeto
+        if EstadoAtual == "testado": 
+            return "não aceito" # Caso de não haver a transição 
     for finais in EstadoFinalAFD:
         if finais == EstadoAtual:
-            return "aceito"
-    return "não aceito"
+            return "aceito" # Teste para ver se esta no estado final
+    return "não aceito" # Se não
 
 def testar_texto(arquivo):
     with open(arquivo + ".txt",'r') as palavras:
         with open(arquivo + "_Resultado.txt", 'x') as resultado:
             for palavra in palavras:
-                resultado.write(palavra.strip() + " " + maquina_AFD(palavra.strip()) + "\n")
+                resultado.write(palavra.strip() + " " + maquina_AFD(palavra.strip()) + "\n") # Testa cada palavra e escreve o resultado 
 
 def limpar_memoria():
     global EstadoFinal, EstadoFinalAFD ,EstadoInicial, EstadoInicialAFD, AFD, AFND
